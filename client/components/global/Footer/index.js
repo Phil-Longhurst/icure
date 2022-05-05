5fdc17d9091fd54690ddea31c46ab1907596c9b0
Phil-Longhurst <phil.longhurst@onqor.co.uk>
2022-05-05 16:59:36 +0100

    footer chnages

--- client/components/global/Footer/index.js
+++ client/components/global/Footer/index.js
@@ -11,36 +11,52 @@ const Footer = ({ footer, pageData }) => {
 
   return (
 			<footer className={styles.footer}>	
-					<div className={styles.footer__news}>
+        <div className={styles.container}>
+          {/* news col 1 */}
+          <div className={styles.footer__news}>
 							<p>	Subscribe to ICURe Newsletter</p>
               <p>Sign up to our newsletter to stay updated with everything ICURe related!</p>
-              <label>Email Address</label>
-              <input type="email" placeholder="email"/>
+              <div>
+                <input type="email" placeholder="email"/>
+                <input type="submit" value="Sign Up"/>
+              </div>
 					</div>
+          {/* nav col 2 */}
           <div  className={styles.footer__nav}>
-            <h4></h4>
+            <h4>Sitemap</h4>
 						<ul id={styles.menu_footer_showcases}>
               <li>hello </li>
               <li>hello </li>
               <li>hello </li>
             </ul>
 					</div>
+          {/* showcases col 3 */}
 					<div className={styles.footer__showcases}>
-            <h4></h4>
+            <h4>Showcases</h4>
             <ul className={styles.menu_footer_showcases}>
               <li>hello </li>
               <li>hello </li>
               <li>hello </li>
             </ul>
 					</div>
-            {/* terms */}
-            <div className={styles.footer__terms}>
+          {/* showcases col 4 */}
+          <div className={styles.footer__showcases}>
+            <h4>Socials</h4>
+            <ul className={styles.menu_footer_showcases}>
+              <li>hello </li>
+              <li>hello </li>
+              <li>hello </li>
+            </ul>
+					</div>
+        </div>
+          {/* terms */}
+          <div className={styles.footer__terms}>
               <div className="container space-between">
                 <p className={styles.footer__terms__date}>© ImpactFlow 2022</p>
                 <p className={styles.footer__terms__terms}><a href="<?php echo get_field('terms', 'option'); ?>" target="blank">Terms of Service</a></p>
               </div>
             </div>
-            {/* onqor */}
+          {/* onqor */}
             <div className={styles.footer__onqor}>
               <div className="container space-between">
                 <p>Site Designed &amp; Built By <a href="https://onqor.co.uk" target="_blank">ONQOR Ltd.</a></p>	
